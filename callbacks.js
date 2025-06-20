4eeab0537c148b82d384bc794be09d949596e81b
Sanika1011 <desaisanika501@gmail.com>
2025-06-20 20:57:43 +0530

    Add files via upload

---
+++ callbacks.js
@@ -0,0 +1,287 @@
+function getdata(dataId) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("data", dataId);
+            resolve("success")
+        }, 2000);
+    });
+}
+
+
+// async function getalldata(){
+//     console.log("getting data1........");
+//     await getdata(1);
+//     console.log("getting data2........");
+//     await getdata(2);
+//     console.log("getting data3........");
+//     await getdata(3);
+//     console.log("getting data4........");
+//     await getdata(4);
+//     console.log("getting data5........");
+//     await getdata(5);
+// }
+
+
+// iife
+
+// (function () {
+//     // …
+//   })();
+  
+//   (() => {
+//     // …
+//   })();
+  
+//   (async () => {
+//     // …
+//   })();
+
+
+(async function (){
+    console.log("getting data1........");
+    await getdata(1);
+    console.log("getting data2........");
+    await getdata(2);
+    console.log("getting data3........");
+    await getdata(3);
+    console.log("getting data4........");
+    await getdata(4);
+    console.log("getting data5........");
+    await getdata(5);
+}) ();
+
+// function api(){
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("weather data");
+//             // 200 is used for showing succes
+//             resolve(200);   
+//         }, 2000);
+//     })
+// }
+
+// async function getweatherdata() {
+//     await api();  //1st call
+//     await api();  //2nd call
+// }
+
+
+
+// --------------------------------Promise-chaining-------------------------------------------
+// function asyncFunc1() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("data1");
+//             resolve("success1");
+//             // reject("erorr");
+//         }, 4000);
+//     });
+// }
+
+
+// function asyncFunc2() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("data2");
+//             resolve("success2")
+//         }, 4000);
+//     })
+// }
+
+// console.log("fetching data....");
+// asyncFunc1().then((res) => {
+//     console.log("fetching data2...");
+//     asyncFunc2().then((res) => {
+//         // console.log(res);
+//     });
+// });
+
+
+
+
+
+
+// ----------------------------------Promise--------------------------------------------------- 
+// function asyncFunc1() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("data1");
+//             resolve("success1");
+//             // reject("erorr");
+//         }, 4000);
+//     });
+// }
+
+
+// function asyncFunc2() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("data2");
+//             resolve("success2")
+//         }, 4000);
+//     })
+// }
+
+// console.log("fetching data....");
+// let p1 = asyncFunc1();
+// p1.then((res) => {
+//     console.log(res);
+// });
+
+// console.log("fetching data2...");
+// let p2 = asyncFunc2();
+// p2.then((res) => {
+//     console.log(res);
+// });
+
+
+// p1.catch((err) => {
+//     console.log(err);
+// });
+
+
+
+
+
+// let promise = new Promise((resolve, reject) =>{
+//     console.log("i am a promise.");
+    // resolve("successfull");
+//     reject("some error occurred");
+// });
+
+// function getdata(dataId, getnextdata) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             // console.log("data", dataId);
+//             // resolve("success");
+//             reject("error");
+//             if(getnextdata) {
+//                 getnextdata();
+//             }
+//         }, 5000);
+//     });
+// }
+ 
+// const getpromise = () => {
+//         return new Promise((resolve, reject) => {
+//             console.log("I am a promise.");
+//             resolve("success");
+//             // reject("network error");
+//         })
+// } 
+
+// let promise = getpromise();
+// promise.then((res)=>{
+//     console.log("promise fullfilled", res); 
+// })
+
+
+// promise.catch((err) => {
+//     console.log("rejected", err); 
+// });
+
+
+
+
+
+
+
+// -------------------------------callback function------------------------------------
+// {1}
+// function sum(a, b) {
+//     console.log(a  + b);
+// }
+
+// function calculator(a, b, sumcallback){
+//     sumcallback(a,b);
+// }
+
+// calculator(1,2,sum);
+
+
+// {2}
+// const hello = () => {
+//     console.log("hello");
+// }
+
+// setTimeout(hello,3000);
+
+
+// {3}
+// function dohomework(subject, callback){
+//     console.log(`starting my ${subject} hw`);
+//         callback();
+// }
+
+// function completehw(){
+//     console.log('finished hw.');
+// }
+
+// dohomework('Math', completehw);
+
+
+// {4}
+// function greet(name, callback){
+//     console.log(`my name is ${name}`);
+//     callback();
+// }
+
+// function good() {
+//     console.log("by by....")
+// }
+
+// greet('sanika', good)
+
+// {5}
+// function delaymsg(message, delay, callback){
+//     setTimeout(function() {
+//         console.log(message);
+//         callback();
+//     }, delay);
+// }
+
+// function afterdelay(){
+//     console.log("after delay");
+// }
+
+// delaymsg("this function is late", 2000, afterdelay)
+
+
+// {6}
+// function askUser(question, yescallback, nocallback){
+//     const answer =  confirm(question);
+//     if(answer){
+//         yescallback();
+//     }
+//     else{
+//         nocallback();
+//     }
+// }
+
+// function sayyes(){
+//     console.log("user clicked ok.");    
+// }
+
+// function sayno(){
+//     console.log("user clicked cancel.");
+// }
+
+// askUser("do u want to confirm the meet?", sayyes, sayno)
+
+
+// function getdata(dataId, getnextdata) {
+//     setTimeout(() => {
+//         console.log("data", dataId);
+//         if(getnextdata) {
+//             getnextdata();
+//         }
+//     }, 2000);
+// }
+
+// getdata(1,() => {
+//     getdata(2, () => {
+//         getdata(3, () => {
+//             getdata(4);
+//         })
+//     })
+// });
---
+++ script.js
@@ -0,0 +1,145 @@
+// if we are handling events inline and in js then the priority will be for the js event handling
+//  node.event = ( ) => {
+     //handle here
+//  }
+
+// let btn = document.querySelector("button");
+
+// btn.onclick = () => {
+//     console.log("button was clicked");
+    // let a = 25;
+    // a++;
+    // console.log(a);
+// }; 
+
+
+let btn2 = document.querySelector("#btn2");
+
+btn2.ondblclick = () => {
+    console.log("btn was clicked 2 times!!");
+}; 
+
+
+// let div = document.querySelector(".div1");
+
+// div.onmouseover = () => {
+//     console.log("u r inside the div");
+// };
+
+
+
+// Change the background color of a <div> when the page loads.
+
+// let div2 = document.querySelector(".div2");
+
+// div2.onload = () => {
+//     console.log("heyy");
+//     // div2.style.backgroundColor = "yellow";
+// };
+
+// window.addEventListener("load", function () {
+//   // Select the div
+//   const div2 = document.getElementById("div");
+
+//   // Change the background color
+//   div2.style.backgroundColor = "lightblue";
+// });
+
+
+
+// event objects
+
+//  node.event = (e) => {
+//  //handle here
+//  }
+
+let div = document.querySelector("div");
+div.onmouseover = (evt) => {
+    console.log("u r inside the div");
+    console.log(evt);
+    console.log(evt.type);
+    console.log(evt.target);
+    console.log(evt.clientX , evt.clientY,);
+}
+
+
+
+// on event listners
+
+
+let btn = document.querySelector("button");
+
+btn.addEventListener("click" , (evt) => {
+    console.log("button1 was clicked - handler1");
+});
+
+
+btn.addEventListener("click" ,  (evt) => {
+    console.log("button1 was clicked - handler2");
+});
+
+const handler3 = () => {
+     console.log("button1 was clicked - handler3");
+};
+
+btn.addEventListener("click" , handler3);
+
+btn.addEventListener("click" ,  (evt) => {
+    console.log("button1 was clicked - handler4");
+});
+
+btn.removeEventListener("click" , handler3);
+
+
+
+// Add a button that changes the text of a paragraph when clicked.
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("changeparatext").addEventListener("click", function () {
+    document.getElementById("mypara").innerText = "This is the changed text!!";
+  });
+});
+
+
+// Create an input box and a button. When the button is clicked, alert the value of the input.
+
+let inpbox = document.createElement("input");
+inpbox.type = "text";
+
+let inpbtn = document.createElement("button");
+inpbtn.innerText = "Show Value";
+
+document.querySelector("body").append(inpbox,inpbtn);
+
+inpbtn.addEventListener("click" , () => {
+    alert(inpbox.value);
+});
+
+
+
+// Add or remove a class from an element on button click.
+
+// let btn3 = document.getElementById("btnclass");
+
+// btn3.addEventListener("click" , () => {
+//     btn3.classList.add("newbtnclass");
+// });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  let btn3 = document.getElementById("btnclass");
+
+  if (btn3) {
+    btn3.addEventListener("click", () => {
+      btn3.classList.add("newbtnclass");
+    });
+  }
+});
+
+
+
+
+// Toggle the visibility of an element using display: none.
+
+
+// sanika desai good girl ahe ti chaha krte 
